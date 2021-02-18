d5d1845a560fd7949f34dfac63206758fd23d304
Daniel Costelloe <danny.costelloe@gmail.com>
2021-02-18 15:44:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
 
-def mario(status)
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+def mario
+  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
